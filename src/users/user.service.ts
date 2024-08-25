5f5e7933dc0787cc25cb7bca2848c67cf20c6497
Ericaltez <ericaltez1111@gmail.com>
2024-08-25 19:36:15 -0300

    add utils folder containing bcypt function, mod entity email column to be unique, code breaks if user email already in use

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entity/user.entity';
+import { hashPassword } from 'src/utils/bcrypt';
 
 @Injectable()
 export class UserService {
@@ -11,17 +12,20 @@ export class UserService {
     private usersRepository: Repository<User>,
   ) {}
 
+  async createUser(userData: CreateUserDto): Promise<User> {
+    // mod the dto or copy?
+    const hashedPassword = await hashPassword(userData.password);
+    userData.password = hashedPassword;
+    const createdUser = this.usersRepository.create(userData);
+    return this.usersRepository.save(createdUser);
+  }
+
   async findAll(): Promise<User[]> {
     return await this.usersRepository.find();
   }
 
   async findById(id: number): Promise<User | null> {
     return await this.usersRepository.findOneBy({ id });
-  }
-
-  async createUser(userData: CreateUserDto): Promise<User> {
-    const createdUser = this.usersRepository.create(userData);
-    return this.usersRepository.save(createdUser);
   }
 
   async delete(id: number): Promise<void> {
